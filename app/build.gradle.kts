0ffeba52396748dbf4b4d4c4c82c483ac2ee22eb
Peter <hoangnm9a2@gmail.com>
2023-10-26 01:38:44 +0700

    update thu vien

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -44,7 +43,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database-ktx")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("androidx.cardview:cardview:1.0.0")
-
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.google.firebase:firebase-storage-ktx")
 }
