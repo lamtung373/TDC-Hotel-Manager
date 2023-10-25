bbcfeb416b67c4c3cb8be4d2bdf3b1ffc0bf632e
baotruongmama123 <baotruongmama123@gmail.com>
2023-10-26 00:36:24 +0700

    Màn hình quản lý lịch làm

---
+++ app/src/main/java/com/example/tdchotel_manager/Menu_QuanLy/QuanLyLichLam.java
@@ -0,0 +1,16 @@
+package com.example.tdchotel_manager.Menu_QuanLy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tdchotel_manager.R;
+
+public class QuanLyLichLam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quan_ly_lich_lam);
+    }
+}
