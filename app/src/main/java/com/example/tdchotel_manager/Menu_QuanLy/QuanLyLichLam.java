50fa54850588d0692ded20040681114bd354b581
baotruongmama123 <baotruongmama123@gmail.com>
2023-10-30 16:52:19 +0700

    Cập nhật bảng lịch làm

--- app/src/main/java/com/example/tdchotel_manager/Menu_QuanLy/QuanLyLichLam.java
+++ app/src/main/java/com/example/tdchotel_manager/Menu_QuanLy/QuanLyLichLam.java
@@ -1,16 +1,149 @@
 package com.example.tdchotel_manager.Menu_QuanLy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.tdchotel_manager.LichLamAdapter;
+import com.example.tdchotel_manager.Model.ca_lam;
+import com.example.tdchotel_manager.Model.chuc_vu;
+import com.example.tdchotel_manager.Model.nhan_vien;
+import com.example.tdchotel_manager.Model.phan_cong;
 import com.example.tdchotel_manager.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuanLyLichLam extends AppCompatActivity {
+
+    private RecyclerView rcvLichLam;
+    List<ca_lam> caLamList = new ArrayList<>();
+    List<nhan_vien> nhanVienList = new ArrayList<>();
+    List<chuc_vu> chucVuList = new ArrayList<>();
+    private LichLamAdapter lichLamAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quan_ly_lich_lam);
+        setControl();
+        setEvent();
+    }
+
+    private void setEvent() {
+
+    }
+
+    private void LoadChucVu() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("chuc_vu");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    chuc_vu chucVu = dataSnapshot.getValue(chuc_vu.class);
+                    chucVuList.add(chucVu);
+
+                }
+                lichLamAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void LoadNhanVien() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("nhan_vien");
+        DatabaseReference ref1 = FirebaseDatabase.getInstance().getReference("phan_cong");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    nhan_vien nhan_vien = dataSnapshot.getValue(nhan_vien.class);
+
+                    String idNhanVien = nhan_vien.getId_nhan_vien();
+
+
+                    Query query = ref1.child("id_nhan_vien").equalTo(idNhanVien);
+                    query.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            List<phan_cong> phanCongList = new ArrayList<>();
+
+                            for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                                phan_cong phan_cong = snapshot1.getValue(phan_cong.class);
+                                phanCongList.add(phan_cong);
+                                Log.e("Test"+phanCongList.size(),"");
+                            }
+                            nhan_vien.setPhanCongList(phanCongList);
+                            nhanVienList.add(nhan_vien);
+
+                            lichLamAdapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+
+
+                lichLamAdapter.notifyDataSetChanged();
+
+                // getListNhanVien(nhanVienList);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(String.valueOf(nhanVienList.size()) + " hao", "");
+            }
+        });
+    }
+
+    private void getListNhanVien(List<nhan_vien> nhanVienList1) {
+        nhanVienList.addAll(nhanVienList1);
+    }
+
+    private void setControl() {
+        rcvLichLam = findViewById(R.id.rcvLichLam);
+
+
+        LoadNhanVien();
+        LoadChucVu();
+
+//        ArrayList<String>  CCCD = new ArrayList<>();
+//        String a1 = "fdasdsf";
+//        CCCD.add(a1);
+//        phan_cong phan_cong = new phan_cong("1","1","2");
+//        List<phan_cong> phanCongList = new ArrayList<>();
+//        phanCongList.add(phan_cong);
+//        nhan_vien nv = new nhan_vien("1","1","232","eafsd","fasdf","fdasfa",CCCD,"dfasfdfd",2332);
+//        nv.setPhanCongList(phanCongList);
+//        nhanVienList.add(nv);
+//        chuc_vu chucVu = new chuc_vu("1","Bảo");
+//        chucVuList.add(chucVu);
+        lichLamAdapter = new LichLamAdapter(nhanVienList, chucVuList, caLamList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rcvLichLam.setLayoutManager(linearLayoutManager);
+        RecyclerView.ItemDecoration decoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rcvLichLam.addItemDecoration(decoration);
+        rcvLichLam.setAdapter(lichLamAdapter);
     }
 }
--- app/src/main/java/com/example/tdchotel_manager/Model/phan_cong.java
+++ app/src/main/java/com/example/tdchotel_manager/Model/phan_cong.java
@@ -1,69 +1,42 @@
 package com.example.tdchotel_manager.Model;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 public class phan_cong {
-    int id_nhan_vien,id_ca_lam;
-    Date ngay_lam,check_in,check_out;
-    public phan_cong( int id_nhan_vien, int id_ca_lam, Date ngay_lam, Date check_in, Date check_out) {
+    String id_phan_cong;
+    String id_nhan_vien;
+    String id_ca_lam;
+    int dayofweek;
+
+    public phan_cong(String id_phan_cong, String id_nhan_vien, String id_ca_lam, int dayofweek) {
+        this.id_phan_cong = id_phan_cong;
         this.id_nhan_vien = id_nhan_vien;
         this.id_ca_lam = id_ca_lam;
-        this.ngay_lam = ngay_lam;
-        this.check_in = check_in;
-        this.check_out = check_out;
-    }
-    public Map<String, Object> toMap() {
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("id_nhan_vien", id_nhan_vien);
-        result.put("id_ca_lam", id_ca_lam);
-        result.put("ngay_lam", ngay_lam);
-        result.put("check_in", check_in);
-        result.put("check_out", check_out);
-
-        return result;
-    }
-    public phan_cong() {
+        this.dayofweek = dayofweek;
     }
 
-    public int getId_nhan_vien() {
+    public String getId_nhan_vien() {
         return id_nhan_vien;
     }
 
-    public void setId_nhan_vien(int id_nhan_vien) {
+    public void setId_nhan_vien(String id_nhan_vien) {
         this.id_nhan_vien = id_nhan_vien;
     }
 
-    public int getId_ca_lam() {
+    public String getId_ca_lam() {
         return id_ca_lam;
     }
 
-    public void setId_ca_lam(int id_ca_lam) {
+    public void setId_ca_lam(String id_ca_lam) {
         this.id_ca_lam = id_ca_lam;
     }
 
-    public Date getNgay_lam() {
-        return ngay_lam;
+    public int getDayofweek() {
+        return dayofweek;
     }
 
-    public void setNgay_lam(Date ngay_lam) {
-        this.ngay_lam = ngay_lam;
+    public void setDayofweek(int dayofweek) {
+        this.dayofweek = dayofweek;
     }
 
-    public Date getCheck_in() {
-        return check_in;
-    }
-
-    public void setCheck_in(Date check_in) {
-        this.check_in = check_in;
-    }
-
-    public Date getCheck_out() {
-        return check_out;
-    }
-
-    public void setCheck_out(Date check_out) {
-        this.check_out = check_out;
+    public phan_cong() {
     }
 }
