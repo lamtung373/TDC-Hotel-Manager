640b90bd4bcd20bff217b0b0776bf01667cbee92
sangclousi <nguvanup2003@gmail.com>
2023-11-07 16:18:27 +0700

    xử lý layout v3

---
+++ app/src/main/java/com/example/tdchotel_manager/Le_Tan/Adapter_DangSuDung/giahan_thoigian.java
@@ -0,0 +1,15 @@
+package com.example.tdchotel_manager.Le_Tan.Adapter_DangSuDung;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tdchotel_manager.R;
+
+public class giahan_thoigian extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_gia_han_thoi_gian);
+    }
+}
