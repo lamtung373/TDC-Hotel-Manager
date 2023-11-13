e8860ccec0ab235538df2cb914d1be7d8e48c5ac
Peter <hoangnm9a2@gmail.com>
2023-11-13 19:06:40 +0700

    Sửa thời gian chỉ chọn ngày

--- app/src/main/java/com/example/tdchotel_manager/Le_Tan/Activity_XacNhanDatPhongDichVu.java
+++ app/src/main/java/com/example/tdchotel_manager/Le_Tan/Activity_XacNhanDatPhongDichVu.java
@@ -52,6 +52,7 @@ public class Activity_XacNhanDatPhongDichVu extends AppCompatActivity {
     phong phong;
     Date thoi_gian_nhan = null;
     Date thoi_gian_tra = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,84 +64,86 @@ public class Activity_XacNhanDatPhongDichVu extends AppCompatActivity {
     }
 
     private void setEvent() {
-        DatabaseReference reference_hoadon=FirebaseDatabase.getInstance().getReference("hoa_don/"+phong.getId_phong());
+        DatabaseReference reference_hoadon = FirebaseDatabase.getInstance().getReference("hoa_don").child(phong.getId_phong());
         btnXacNhanDV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                    Date now=new Date();
-                    thoi_gian_nhan = new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(btnNgayNhan.getText().toString());
-                    thoi_gian_tra = new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(btnNgayTra.getText().toString());
-                    if(thoi_gian_nhan.before(now)||thoi_gian_nhan.after(thoi_gian_tra)){
+                    Date now = new Date();
+                    thoi_gian_nhan = new SimpleDateFormat("dd/MM/yyyy").parse(btnNgayNhan.getText().toString());
+                    thoi_gian_tra = new SimpleDateFormat("dd/MM/yyyy").parse(btnNgayTra.getText().toString());
+                    if (thoi_gian_nhan.before(now) || thoi_gian_nhan.after(thoi_gian_tra)) {
                         Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Thời gian đã đặt không hợp lệ!!!", Toast.LENGTH_SHORT).show();
                         return;
-                    }
-
-                    else if(((thoi_gian_tra.getTime() - thoi_gian_nhan.getTime()) / (24 * 3600 * 1000))<1){
+                    } else if (((thoi_gian_tra.getTime() - thoi_gian_nhan.getTime()) / (24 * 3600 * 1000)) < 1) {
                         Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Thời gian ở phải ít nhất 1 ngày!!!", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else if (edtSonguoi.getText().toString().trim().equals("0")) {
+                        Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Số lượng khách phải lớn hơn 0!!!", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     reference_hoadon.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            Date thoi_gian_hoadon_nhan=null;
-                            Date thoi_gian_hoadon_tra=null;
-                            for(DataSnapshot dataSnapshot:snapshot.getChildren()){
-                                hoa_don hoa_don=dataSnapshot.getValue(hoa_don.class);
+                            if (snapshot != null || snapshot.getChildrenCount() != 0) {
+                                Date thoi_gian_hoadon_nhan = null;
+                                Date thoi_gian_hoadon_tra = null;
+                                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                    hoa_don hoa_don = dataSnapshot.getValue(hoa_don.class);
 
 
-                                //Lấy những hóa đơn chưa thanh toán
-                                if(hoa_don.getThoi_gian_thanh_toan().equals("")){
-                                    try{
+                                    //Lấy những hóa đơn chưa thanh toán
+                                    if (hoa_don.getThoi_gian_thanh_toan().equals("")) {
+                                        try {
+                                            //kiểm tra thời gian đặt phòng có hợp lệ so với thời gian trong hóa đơn hay không
+
+                                            //Chuyển đổi thời gian
+                                            thoi_gian_hoadon_nhan = new SimpleDateFormat("dd/MM/yyyy").parse(hoa_don.getThoi_gian_nhan_phong().toString());
+                                            thoi_gian_hoadon_tra = new SimpleDateFormat("dd/MM/yyyy").parse(hoa_don.getThoi_gian_tra_phong().toString());
+
+                                            //Kiểm tra thời gian nhận có tồn tại trong thời gian của hóa đơn hay không
+                                            if (thoi_gian_nhan.after(thoi_gian_hoadon_nhan) && thoi_gian_nhan.before(thoi_gian_hoadon_tra) || thoi_gian_nhan.equals(thoi_gian_hoadon_nhan) || thoi_gian_nhan.equals(thoi_gian_hoadon_tra)) {
+                                                Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Đã có lịch", Toast.LENGTH_SHORT).show();
+                                                return;
+                                            }
 
 
-                                        //kiểm tra thời gian đặt phòng có hợp lệ so với thời gian trong hóa đơn hay không
+                                            //Kiểm tra thời gian trả có tồn tại trong thời gian của hóa đơn hay không
+                                            else if (thoi_gian_tra.after(thoi_gian_hoadon_nhan) && thoi_gian_tra.before(thoi_gian_hoadon_tra) || thoi_gian_tra.equals(thoi_gian_hoadon_nhan) || thoi_gian_tra.equals(thoi_gian_hoadon_tra)) {
+                                                Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Đã có lịch", Toast.LENGTH_SHORT).show();
+                                                return;
+                                            }
 
-                                        //Chuyển đổi thời gian
-                                        thoi_gian_hoadon_nhan = new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(hoa_don.getThoi_gian_nhan_phong().toString());
-                                        thoi_gian_hoadon_tra = new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(hoa_don.getThoi_gian_tra_phong().toString());
 
-                                        //Kiểm tra thời gian nhận có tồn tại trong thời gian của hóa đơn hay không
-                                        if(thoi_gian_nhan.after(thoi_gian_hoadon_nhan)&&thoi_gian_nhan.before(thoi_gian_hoadon_tra)||thoi_gian_nhan.equals(thoi_gian_hoadon_nhan)||thoi_gian_nhan.equals(thoi_gian_hoadon_tra)){
-                                            Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Đã có lịch", Toast.LENGTH_SHORT).show();
-                                            return;
+                                            //Kiểm tra thời gian nhận của hóa đơn có tồn tại trong thời gian nhận hoặc trả hay không
+                                            else if (thoi_gian_hoadon_nhan.after(thoi_gian_nhan) && thoi_gian_hoadon_nhan.before(thoi_gian_tra) || thoi_gian_hoadon_nhan.equals(thoi_gian_nhan) || thoi_gian_hoadon_nhan.equals(thoi_gian_tra)) {
+                                                Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Đã có lịch", Toast.LENGTH_SHORT).show();
+                                                return;
+                                            }
+
+
+                                            //Kiểm tra thời gian trả của hóa đơn có tồn tại trong thời gian nhận hoặc trả hay không
+                                            else if (thoi_gian_hoadon_tra.after(thoi_gian_nhan) && thoi_gian_hoadon_nhan.before(thoi_gian_tra) || thoi_gian_hoadon_tra.equals(thoi_gian_nhan) || thoi_gian_hoadon_tra.equals(thoi_gian_tra)) {
+                                                Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Đã có lịch", Toast.LENGTH_SHORT).show();
+                                                return;
+                                            }
+                                        } catch (Exception e) {
+                                            Log.e("Lỗi chuyển đổi dữ liệu thời gian thanh toán", e.getMessage());
                                         }
-
-
-                                        //Kiểm tra thời gian trả có tồn tại trong thời gian của hóa đơn hay không
-                                        else if(thoi_gian_tra.after(thoi_gian_hoadon_nhan)&&thoi_gian_tra.before(thoi_gian_hoadon_tra)||thoi_gian_tra.equals(thoi_gian_hoadon_nhan)||thoi_gian_tra.equals(thoi_gian_hoadon_tra)){
-                                            Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Đã có lịch", Toast.LENGTH_SHORT).show();
-                                            return;
-                                        }
-
-
-                                        //Kiểm tra thời gian nhận của hóa đơn có tồn tại trong thời gian nhận hoặc trả hay không
-                                        else if(thoi_gian_hoadon_nhan.after(thoi_gian_nhan)&&thoi_gian_hoadon_nhan.before(thoi_gian_tra)||thoi_gian_hoadon_nhan.equals(thoi_gian_nhan)||thoi_gian_hoadon_nhan.equals(thoi_gian_tra)){
-                                            Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Đã có lịch", Toast.LENGTH_SHORT).show();
-                                            return;
-                                        }
-
-
-                                        //Kiểm tra thời gian trả của hóa đơn có tồn tại trong thời gian nhận hoặc trả hay không
-                                        else if(thoi_gian_hoadon_tra.after(thoi_gian_nhan)&&thoi_gian_hoadon_nhan.before(thoi_gian_tra)||thoi_gian_hoadon_tra.equals(thoi_gian_nhan)||thoi_gian_hoadon_tra.equals(thoi_gian_tra)){
-                                            Toast.makeText(Activity_XacNhanDatPhongDichVu.this, "Đã có lịch", Toast.LENGTH_SHORT).show();
-                                            return;
-                                        }
-                                        Intent intent=new Intent(Activity_XacNhanDatPhongDichVu.this,Activity_XacNhanDatPhongDichVu.class);
-                                        intent.putExtra("thoi_gian_nhan",btnNgayNhan.getText().toString());
-                                        intent.putExtra("thoi_gian_tra",btnNgayTra.getText().toString());
-                                        intent.putExtra("so_luong_khach",edtSonguoi.getText().toString());
-                                        intent.putExtra("dich_vu_theo_nguoi",adapterDvTheoNguoi.getData_dv());
-                                        intent.putExtra("dich_vu_phong",adapterDvTheoPhong.getData_dv());
-                                      //  startActivity(intent);
-                                        Log.e("dvphong",""+adapterDvTheoPhong.getData_dv().get(0).getTen_dich_vu()+" "+adapterDvTheoPhong.getData_dv().get(0).isCheck());
-                                        Log.e("dvphong",""+adapterDvTheoNguoi.getData_dv().get(0).getTen_dich_vu()+" "+adapterDvTheoNguoi.getData_dv().get(0).getSo_luong());
-                                    }catch (Exception e){
-                                        Log.e("Lỗi chuyển đổi dữ liệu thời gian thanh toán", e.getMessage());
                                     }
-                                }
-                            }
 
+                                }
+                                Intent intent=new Intent(Activity_XacNhanDatPhongDichVu.this,Activity_XacNhanDatPhongDichVu.class);
+                                intent.putExtra("thoi_gian_nhan",btnNgayNhan.getText().toString());
+                                intent.putExtra("thoi_gian_tra",btnNgayTra.getText().toString());
+                                intent.putExtra("so_luong_khach",edtSonguoi.getText().toString());
+                                intent.putExtra("dich_vu_theo_nguoi",adapterDvTheoNguoi.getData_dv());
+                                intent.putExtra("dich_vu_phong",adapterDvTheoPhong.getData_dv());
+                                intent.putExtra("phong",phong);
+                                //  startActivity(intent);
+                                Log.e("dvphong",""+adapterDvTheoPhong.getData_dv().get(0).getTen_dich_vu()+" "+adapterDvTheoPhong.getData_dv().get(0).isCheck());
+                                Log.e("dvphong",""+adapterDvTheoNguoi.getData_dv().get(0).getTen_dich_vu()+" "+adapterDvTheoNguoi.getData_dv().get(0).getSo_luong());
+                            }
                         }
 
                         @Override
@@ -148,7 +151,8 @@ public class Activity_XacNhanDatPhongDichVu extends AppCompatActivity {
 
                         }
                     });
-                } catch (Exception e) {
+                } catch (
+                        Exception e) {
                     Log.e("Lỗi chuyển đổi dữ liệu thời gian đã đặt", e.getMessage());
                 }
             }
@@ -204,16 +208,8 @@ public class Activity_XacNhanDatPhongDichVu extends AppCompatActivity {
                         calendar.set(Calendar.YEAR, year);
                         calendar.set(Calendar.MONTH, month);
                         calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                                calendar.set(Calendar.MINUTE, minute);
-                                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-                                btnThoigian.setText(dateFormat.format(calendar.getTime()));
-                            }
-                        };
-                        new TimePickerDialog(Activity_XacNhanDatPhongDichVu.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                        btnThoigian.setText(dateFormat.format(calendar.getTime()));
                     }
                 };
                 new DatePickerDialog(Activity_XacNhanDatPhongDichVu.this, dataListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
