51793a0a8bc3edd2f17303e8e136112a4afeaed4
sangclousi <nguvanup2003@gmail.com>
2023-11-20 12:46:36 +0700

    sửa hóa đơn và dịch vu v9

--- app/src/main/java/com/example/tdchotel_manager/Le_Tan/Adapter_DangSuDung/adapter_dvnguoi.java
+++ app/src/main/java/com/example/tdchotel_manager/Le_Tan/Adapter_DangSuDung/adapter_dvnguoi.java
@@ -1,18 +1,23 @@
 package com.example.tdchotel_manager.Le_Tan.Adapter_DangSuDung;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tdchotel_manager.Le_Tan.Adapter.Adapter_DVTheoNguoi;
+import com.example.tdchotel_manager.Model.chi_tiet_hoa_don_dich_vu;
+import com.example.tdchotel_manager.Model.chi_tiet_tien_nghi;
 import com.example.tdchotel_manager.Model.dich_vu;
 import com.example.tdchotel_manager.Model.loai_dich_vu;
+import com.example.tdchotel_manager.Model.tien_nghi;
 import com.example.tdchotel_manager.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,6 +29,7 @@ import java.util.ArrayList;
 
 public class adapter_dvnguoi extends RecyclerView.Adapter<adapter_dvnguoi.DV_TheoNguoi_Holder> {
     ArrayList<dich_vu> data_dv=new ArrayList<>();
+ArrayList<chi_tiet_hoa_don_dich_vu> chiTietHoaDonDichVus = new ArrayList<>();
 
     public ArrayList<dich_vu> getData_dv() {
         return data_dv;
@@ -49,9 +55,57 @@ public class adapter_dvnguoi extends RecyclerView.Adapter<adapter_dvnguoi.DV_The
         if(!data_dv.isEmpty()&&data_dv!=null) {
             holder.tv_dv.setText(data_dv.get(position).getTen_dich_vu());
             holder.tvGia.setText(data_dv.get(position).getGia_dich_vu() + "đ/người");
-
+            holder.ivCong.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int soluong = Integer.parseInt(holder.edtSonguoi.getText().toString().trim()) + 1;
+                    holder.edtSonguoi.setText(soluong + "");
+                    data_dv.get(holder.getAdapterPosition()).setSo_luong(soluong);
+                }
+            });
+            holder.ivTru.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int soluong = Integer.parseInt(holder.edtSonguoi.getText().toString().trim()) - 1;
+                    if (soluong >= 0) {
+                        holder.edtSonguoi.setText(soluong + "");
+                        data_dv.get(holder.getAdapterPosition()).setSo_luong(soluong);
+                    }
+                }
+            });
         }
     }
+    public void GoiDuLieu(String idHoaDon) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("chi_tiet_hoa_don_dich_vu").child(idHoaDon);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot facilitySnapshot : dataSnapshot.getChildren()) {
+                    chi_tiet_hoa_don_dich_vu comfortDetail = facilitySnapshot.getValue(chi_tiet_hoa_don_dich_vu.class);
+                    if (comfortDetail != null) {
+                        chiTietHoaDonDichVus.add(comfortDetail);
+                        //Log.e("ịgiejifejifjei","-"+comfortDetail.getSo_luong());
+                        Log.e("ịgiejifejifjei","size"+data_dv.size());
+
+                    }
+
+                    for (int i=0;i<data_dv.size();i++){
+                        if(comfortDetail.getId_dich_vu().equals(data_dv.get(i).getId_dich_vu())){
+                            data_dv.get(i).setSo_luong(comfortDetail.getSo_luong());
+                        }
+                    }
+                }
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Xử lý lỗi
+                Log.e("fetchComfortDetails", "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+
 
     @Override
     public int getItemCount() {
@@ -61,11 +115,14 @@ public class adapter_dvnguoi extends RecyclerView.Adapter<adapter_dvnguoi.DV_The
     class DV_TheoNguoi_Holder extends RecyclerView.ViewHolder {
         TextView tv_dv, tvGia;
         EditText edtSonguoi;
+        ImageView ivTru, ivCong;
 
         public DV_TheoNguoi_Holder(@NonNull View itemView) {
             super(itemView);
             tv_dv = itemView.findViewById(R.id.tvdichvu);
             edtSonguoi = itemView.findViewById(R.id.edtSonguoi);
+            ivTru = itemView.findViewById(R.id.ivTru);
+            ivCong = itemView.findViewById(R.id.ivCong);
             tvGia = itemView.findViewById(R.id.tvGia);
         }
     }
@@ -90,10 +147,16 @@ public class adapter_dvnguoi extends RecyclerView.Adapter<adapter_dvnguoi.DV_The
                                             dich_vu dichVu = dataSnapshot.getValue(dich_vu.class);
                                             if (dichVu.getId_loai_dich_vu().equals(loaiDichVu.getId_loai_dich_vu())) {
                                                 data_dv.add(dichVu);
+                                                for(dich_vu dichVu1 : data_dv){
+                                                    if(dichVu1.getId_dich_vu().equals(dichVu.getId_dich_vu()) ){
+                                                        dichVu1.setSo_luong(dichVu.getSo_luong());
+                                                    }
+                                                }
+
                                             }
                                         }
                                         notifyDataSetChanged();
-
+                                        Log.e("sizzzz",data_dv.size()+"");
                                     }
                                 }
 
--- app/src/main/java/com/example/tdchotel_manager/Le_Tan/Adapter_DangSuDung/dichvu_letan.java
+++ app/src/main/java/com/example/tdchotel_manager/Le_Tan/Adapter_DangSuDung/dichvu_letan.java
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.tdchotel_manager.Le_Tan.Activity_XacNhanDatPhongDichVu;
 import com.example.tdchotel_manager.Le_Tan.Adapter.Adapter_DVTheoNguoi;
 import com.example.tdchotel_manager.Le_Tan.Adapter.Adapter_DVTheoPhong;
+import com.example.tdchotel_manager.Model.chi_tiet_hoa_don_dich_vu;
 import com.example.tdchotel_manager.Model.chi_tiet_hoa_don_dich_vu_phong;
 import com.example.tdchotel_manager.Model.dich_vu;
 import com.example.tdchotel_manager.Model.hoa_don;
@@ -36,19 +37,31 @@ import java.util.Date;
 public class dichvu_letan extends AppCompatActivity {
     ArrayList<dich_vu> data_dvphong = new ArrayList<>();
     ArrayList<dich_vu> data_dvtheonguoi = new ArrayList<>();
+String idHoaDon;
+    ArrayList<chi_tiet_hoa_don_dich_vu> chiTietHoaDonDichVus = new ArrayList<>();
     Button btnXacNhanDV;
     RecyclerView rcv_dvphong, rcv_dvtheonguoi;
-    Adapter_DVTheoNguoi adapterDvTheoNguoi = new Adapter_DVTheoNguoi();
-    Adapter_DVTheoPhong adapterDvTheoPhong = new Adapter_DVTheoPhong();
-    com.example.tdchotel_manager.Model.phong phong;
+    adapter_dvnguoi adapterDvTheoNguoi = new adapter_dvnguoi();
+    adapter_dvphong adapterDvTheoPhong = new adapter_dvphong();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_themdichvu_le_tan);
+        Intent intent = getIntent();
+        idHoaDon = intent.getStringExtra("idHoaDon");
+//        phong = (phong) getIntent().getSerializableExtra("phong");
         setControl();
-        phong = (phong) getIntent().getSerializableExtra("phong");
-        Initialization();
         setEvent();
+        if (idHoaDon!=null){
+            fill_data(idHoaDon);
+            Toast.makeText(this,idHoaDon,Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+    void fill_data(String idHoaDon){
+        adapterDvTheoNguoi.GoiDuLieu(idHoaDon);
     }
 
     private void setEvent() {
@@ -63,58 +76,20 @@ public class dichvu_letan extends AppCompatActivity {
                     // Lấy danh sách dịch vụ theo phòng đã chọn
                     ArrayList<dich_vu> dvTheoPhong = adapterDvTheoPhong.getData_dv();
 
-                    // Kiểm tra nếu không có dịch vụ nào được chọn thì thông báo
-                    if (dvTheoNguoi.isEmpty() && dvTheoPhong.isEmpty()) {
-                        Toast.makeText(dichvu_letan.this, "Vui lòng chọn ít nhất một dịch vụ", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
+//                    // Kiểm tra nếu không có dịch vụ nào được chọn thì thông báo
+//                    if (dvTheoNguoi.isEmpty() && dvTheoPhong.isEmpty()) {
+//                        Toast.makeText(dichvu_letan.this, "Vui lòng chọn ít nhất một dịch vụ", Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
 
-                    // Lưu chi tiết hóa đơn dịch vụ vào Firebase
-                    saveChiTietHoaDonDichVuToFirebase(dvTheoNguoi, dvTheoPhong);
                 } catch (Exception e) {
                     Log.e("Lỗi chuyển đổi dữ liệu số lượng khách", e.getMessage());
                 }
             }
         });
-    }
-    private void saveChiTietHoaDonDichVuToFirebase(ArrayList<dich_vu> dvTheoNguoi, ArrayList<dich_vu> dvTheoPhong) {
-        DatabaseReference chiTietHoaDonDichVuRef = FirebaseDatabase.getInstance().getReference("chi_tiet_hoa_don_dich_vu_phong");
 
-        // Lấy mã hóa đơn hoặc tạo mới nếu cần
-        String maHoaDon = chiTietHoaDonDichVuRef.push().getKey(); // Tạo một ID hóa đơn mới
 
-        // Lưu chi tiết hóa đơn dịch vụ theo người
-        for (dich_vu dv : dvTheoNguoi) {
-            chi_tiet_hoa_don_dich_vu_phong chiTietHoaDonDichVu = new chi_tiet_hoa_don_dich_vu_phong();
-            chiTietHoaDonDichVu.setId_hoa_don(maHoaDon); // Liên kết với hóa đơn
-            chiTietHoaDonDichVu.setId_dich_vu_phong(dv.getId_dich_vu());
-            chiTietHoaDonDichVu.setSo_luong(dv.getSo_luong());
 
-            // Tạo một ID duy nhất cho chi tiết hóa đơn dịch vụ
-            String chiTietHoaDonDichVuId = chiTietHoaDonDichVuRef.push().getKey();
-
-            // Lưu chi tiết hóa đơn dịch vụ vào Firebase
-            chiTietHoaDonDichVuRef.child(chiTietHoaDonDichVuId).setValue(chiTietHoaDonDichVu);
-        }
-
-        // Lưu chi tiết hóa đơn dịch vụ theo phòng
-        for (dich_vu dv : dvTheoPhong) {
-            chi_tiet_hoa_don_dich_vu_phong chiTietHoaDonDichVu = new chi_tiet_hoa_don_dich_vu_phong();
-            chiTietHoaDonDichVu.setId_hoa_don(maHoaDon); // Liên kết với hóa đơn
-            chiTietHoaDonDichVu.setId_dich_vu_phong(dv.getId_dich_vu());
-            chiTietHoaDonDichVu.setSo_luong(dv.getSo_luong());
-
-            // Tạo một ID duy nhất cho chi tiết hóa đơn dịch vụ
-            String chiTietHoaDonDichVuId = chiTietHoaDonDichVuRef.push().getKey();
-
-            // Lưu chi tiết hóa đơn dịch vụ vào Firebase
-            chiTietHoaDonDichVuRef.child(chiTietHoaDonDichVuId).setValue(chiTietHoaDonDichVu);
-        }
-
-        Toast.makeText(dichvu_letan.this, "Đã lưu chi tiết hóa đơn dịch vụ", Toast.LENGTH_SHORT).show();
-    }
-
-    private void Initialization() {
         rcv_dvtheonguoi.setLayoutManager(new LinearLayoutManager(dichvu_letan.this, LinearLayoutManager.VERTICAL, false));
         rcv_dvtheonguoi.addItemDecoration(new DividerItemDecoration(dichvu_letan.this, DividerItemDecoration.VERTICAL));
         rcv_dvtheonguoi.setAdapter(adapterDvTheoNguoi);
@@ -123,6 +98,7 @@ public class dichvu_letan extends AppCompatActivity {
         rcv_dvphong.addItemDecoration(new DividerItemDecoration(dichvu_letan.this, DividerItemDecoration.VERTICAL));
         rcv_dvphong.setAdapter(adapterDvTheoPhong);
     }
+
 
     private void setControl() {
         btnXacNhanDV = findViewById(R.id.btnXacNhanDV);
